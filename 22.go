470cf27c88ab4237cb43f6509bfdf5d320b014ab
gbochora <g.bochorishvili@freeuni.edu.ge>
2021-10-12 12:09:29 +0400

    Add problem 22(Generate Parentheses) solution

---
+++ 22.go
@@ -0,0 +1,22 @@
+package main
+
+func generateParenthesis(n int) []string {
+	list := make([]string, 0)
+	comb := make([]byte, 2*n)
+	generateParenthesisRec(0, n, n, 0, comb, &list)
+	return list
+}
+
+func generateParenthesisRec(balance, leftNum, rightNum, index int, comb []byte, list *[]string) {
+	if balance < 0 || leftNum < 0 || rightNum < 0 {
+		return
+	}
+	if balance == 0 && leftNum == 0 && rightNum == 0 {
+		*list = append(*list, string(comb))
+		return
+	}
+	comb[index] = '('
+	generateParenthesisRec(balance+1, leftNum+1, rightNum, index+1, comb, list)
+	comb[index] = ')'
+	generateParenthesisRec(balance-1, leftNum, rightNum+1, index+1, comb, list)
+}
