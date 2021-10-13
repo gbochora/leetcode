5938f0293ac4e6f5167c06bcd9eb22bbfde9a692
gbochora <g.bochorishvili@freeuni.edu.ge>
2021-10-13 11:52:20 +0400

    Add problem 374(Guess Number Higher or Lower) solution

---
+++ 374.go
@@ -0,0 +1,24 @@
+package main
+
+var pick = 10
+
+func guessNumber(n int) int {
+	return binarySearch(1, n)
+}
+
+func binarySearch(left, right int) int {
+	mid := (left + right) / 2
+	res := guess(mid)
+	if res == 0 {
+		return mid
+	}
+	if res < 0 {
+		return binarySearch(left, mid-1)
+	} else {
+		return binarySearch(mid+1, right)
+	}
+}
+
+func guess(num int) int {
+	return pick - num
+}
