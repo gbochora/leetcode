4788163e711722dc6f02018d3203c88d53284383
gbochora <g.bochorishvili@freeuni.edu.ge>
2021-10-23 10:16:53 +0400

    Add 451(sort characters by frequency) solution

---
+++ 451_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFrequencySort(t *testing.T) {
+	got := frequencySort("treerr")
+	want := "rrreet"
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
